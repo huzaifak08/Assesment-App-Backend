3d3917319adb601b927c09f60cfb94caeb480da9
huzaifak08 <hk7928042@gmail.com>
2025-05-20 12:52:20 +0500

    different env ALH

--- config/config.ts
+++ config/config.ts
@@ -2,14 +2,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isDev = process.env.NODE_ENV === "development";
+
 const config = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  username: isDev ? process.env.DEV_DB_USERNAME : process.env.LOCAL_DB_USERNAME,
+  password: isDev ? process.env.DEV_DB_PASSWORD : process.env.LOCAL_DB_PASSWORD,
+  database: isDev ? process.env.DEV_DB_NAME : process.env.LOCAL_DB_NAME,
+  host: isDev ? process.env.DEV_DB_HOST : process.env.LOCAL_DB_HOST,
+  port: isDev ? process.env.DEV_DB_PORT : process.env.LOCAL_DB_PORT,
   dialect: "postgres",
-  logging: true,
+  appPort: process.env.APP_PORT || 3000,
 };
 
 export default config;
